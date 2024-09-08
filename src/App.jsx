072dd75925f4738f640bcbd9ced855ca935a12d2
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-09-08 08:11:20 +0530

    fix: "true"

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Router from "./route/Router";
+import Header from "./layouts/Header";
+
+function App() {
+  return (
+    <>
+    <Header/>
+       <Router />
+    </>
+  );
+}
+
+export default App;
